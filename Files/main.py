13751ff7a5f8c155d6f0ea12fb488812d0f03d98
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-07-20 09:54:45 -0500

    Update main.py

--- Files/main.py
+++ Files/main.py
@@ -9,14 +9,18 @@ print("Set up a network drive (on your computer or on another computer) and pres
 #There's no press any key to continue in Python, so let's use input()
 input("")
 #I will stop commenting about these commands, but whenever I use print(), print("This is the text that would be shown on the screen.")
-print("Please have your new Chromebook ready.")
+print("Now, here is some interactive help!")
 input()
-
+print("Now, plug in your Chromebook.")
+input()
+print("Now turn it on.")
+input()
 print("You can setup a network drive in the newly created folder in your Documents, or use another network drive. Please type N for the folder in your Documents, or A for another network drive.")
+#Ask for an option and put it into a variable.
 networkdrive = input("Make your choice now: ")
 print("OK!")
 if networkdrive == "A":
     networkdrivepath = input("Where is that drive located?: ")
 else:
     print(home + "\\Documents\\NetworkDrive")
-
+print("")
