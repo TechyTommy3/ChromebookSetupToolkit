0201582e298a6679bcef8fff1f026adfe5ad0183
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-07-20 07:37:09 -0500

    Update main.py

--- Files/main.py
+++ Files/main.py
@@ -1,3 +1,7 @@
+#Get the user directory
+from pathlib import Path
+home = str(Path.home())
+#Make directory
 #Chromebook Switching Kit
 print("Chromebook Switching Kit")
 #Ask for a network drive
@@ -7,7 +11,12 @@ input("")
 #I will stop commenting about these commands, but whenever I use print(), print("This is the text that would be shown on the screen.")
 print("Please have your new Chromebook ready.")
 input()
+
 print("You can setup a network drive in the newly created folder in your Documents, or use another network drive. Please type N for the folder in your Documents, or A for another network drive.")
 networkdrive = input("Make your choice now: ")
 print("OK!")
+if networkdrive == "A":
+    networkdrivepath = input("Where is that drive located?: ")
+else:
+    print(home + "\\Documents\\NetworkDrive")
 
