428b05b781336a88f8cea8523cefacc51eec1948
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-07-20 07:27:05 -0500

    Add my main files using GitHub Desktop and SyncToy (yes, that SyncToy, the one bundled with XP PowerToys)

---
+++ Files/main.py
@@ -0,0 +1,13 @@
+#Chromebook Switching Kit
+print("Chromebook Switching Kit")
+#Ask for a network drive
+print("Set up a network drive (on your computer or on another computer) and press enter")
+#There's no press any key to continue in Python, so let's use input()
+input("")
+#I will stop commenting about these commands, but whenever I use print(), print("This is the text that would be shown on the screen.")
+print("Please have your new Chromebook ready.")
+input()
+print("You can setup a network drive in the newly created folder in your Documents, or use another network drive. Please type N for the folder in your Documents, or A for another network drive.")
+networkdrive = input("Make your choice now: ")
+print("OK!")
+
